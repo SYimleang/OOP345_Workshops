342f5508249ecf30e391d1aa11ccd808c8b7cf67
syimleang <syimleang@myseneca.ca>
2023-11-17 23:34:52 -0500

    Inititail workshop 9

---
+++ OOP345_Workshops/Workshop_9/part_1/process_data.h
@@ -0,0 +1,30 @@
+// Workshop 9 - Multi-Threading, Thread Class
+// Name:	Sasawat Yimleang
+// ID:		114036221
+// E-mail:	syimleang@myseneca.ca
+// Date:	November 17, 2023
+
+#ifndef SDDS_PROCESSDATA_H
+#define SDDS_PROCESSDATA_H
+
+#include<string>
+
+namespace sdds {
+	void computeAvgFactor(const int* arr, int size, int divisor, double& avg);
+	void computeVarFactor(const int* arr, int size, int divisor, double avg, double& var);
+
+	// Class ProcessData
+	class ProcessData {
+		int total_items{};	// 
+		int* data{};		// 
+	public:
+		ProcessData(const std::string& input_file);
+		ProcessData(const ProcessData&) = delete;
+		ProcessData& operator=(const ProcessData&) = delete;
+		~ProcessData();
+		operator bool() const;
+		int operator()(const std::string& target_file, double& avg, double& var);
+	};
+}
+
+#endif
